0e258b4aa794af00d73ccb8982e1f077f25c0f9f
moo <48740106+moom0o@users.noreply.github.com>
2022-04-23 23:04:24 -0400

    add simple express server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.get('/kh/v=923', function (req, res) {
+    res.sendFile(__dirname + `/images/${req.query.z}/${req.query.x}/${req.query.x}_${req.query.y}.jpeg`)
+})
+
+app.listen(42069)
